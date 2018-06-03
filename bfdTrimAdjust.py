c355a17fce00cc64d48fd4739543e737680f38b2
Dominik Jain <djain@gmx.net>
2018-06-03 20:54:58 +0200

    Fixed replacement, more log output

--- bfdTrimAdjust.py
+++ bfdTrimAdjust.py
@@ -20,20 +20,24 @@ except:
     validParams = False
     
 def replace(m):
-    print "  adjusting trim: '%s' -> '%s'" % (m.group(1), newTrim)
-    return newTrim
+    print "    adjusting trim: '%s' -> '%s'" % (m.group(1), newTrim)
+    return 'dbTrim="%s"' % newTrim
 
 if validParams:
-    for root, dirs, files in os.walk("."):
+    def onerror(e): print e
+    for root, dirs, files in os.walk(".", followlinks=True, onerror=onerror):
+        print root
         for fn in files:
-            if fn == "BFDArticTweaks.xml":
+            if fn.lower()[-3:] == "xml":
                 path = os.path.join(root, fn)
-                print path
-                with file(path, "r") as f:
-                    content = f.read()
-                content = re.sub(r'dbTrim="(.*?)"', replace, content)
-                if adjustTrim:
-                    with file(path, "w") as f:
-                        f.write(content)
+                print "  %s" % path
+                if fn == "BFDArticTweaks.xml":     
+                    print "  %s" % path
+                    with file(path, "r") as f:
+                        content = f.read()
+                    content = re.sub(r'dbTrim="(.*?)"', replace, content)
+                    if adjustTrim:
+                        with file(path, "w") as f:
+                            f.write(content)
 
 raw_input("Execution complete. Press Enter to continue...")
